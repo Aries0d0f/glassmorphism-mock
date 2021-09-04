6603c7fd37edd9baa64535fb8808ea615a081306
Aries Cs <aries0d0f@gmail.com>
2021-09-04 14:36:45 +0800

    feat(view): Implement `Glass` component to simulate a glass flasks.

--- src/components/element/Glass.vue
+++ src/components/element/Glass.vue
@@ -11,8 +11,17 @@
 
 .glass {
   &-container {
-    padding: 16px;
+    padding: 14px 18px 18px 14px;
     display: inline-flex;
+    border-radius: 6px;
+    box-shadow: 3px 4px 5px darken($gray-3, 2.5%),
+          inset -3px -4px 1px darken($gray-3, 10%),
+          inset -3px -4px 50px rgba(darken($gray-3, 10%), 25%),
+          inset -1.5px -2px 50px rgba($white, 25%),
+          -3px -4px 10px rgba($white, 25%),
+          3px 4px 10px rgba($white, 50%),
+          inset 3px 4px 5px lighten($gray-3, 5%),
+          -3px -4px 1px lighten($gray-3, 10%);
   }
 }
 </style>
